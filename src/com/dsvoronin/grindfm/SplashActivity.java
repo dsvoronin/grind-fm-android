b7b55a05f1a996234f5b8d2deb53ee99f33eb39a
dsvoronin <dsvoronin@gmail.com>
2012-02-13 21:37:27 +0400

    splash screen try

---
+++ src/com/dsvoronin/grindfm/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.dsvoronin.grindfm;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.dsvoronin.R;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+    }
+}
