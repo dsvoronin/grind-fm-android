38c0f581999a65cb6860d4aa614aebfd4984ae65
dsvoronin <dsvoronin@gmail.com>
2012-02-13 22:59:49 +0400

    player first strings

--- src/com/dsvoronin/grindfm/SplashActivity.java
+++ src/com/dsvoronin/grindfm/SplashActivity.java
@@ -1,13 +1,78 @@
 package com.dsvoronin.grindfm;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import com.dsvoronin.R;
 
+import java.io.IOException;
+
 public class SplashActivity extends Activity {
+
+    private static final int MEDIA_NOT_READY = 0;
+    private static final int MEDIA_READY = 1;
+    private static final int MEDIA_PLAYING = 2;
+
+    private static final String TAG = SplashActivity.class.getSimpleName();
+
+    private int state = MEDIA_NOT_READY;
+    private Button button;
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.splash);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+        button = (Button) findViewById(R.id.button);
+
+        final MediaPlayer mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource(this, Uri.parse("http://radio.goha.ru:8000/grindfm.ogg"));
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mediaPlayer) {
+                    button.setBackgroundResource(android.R.drawable.ic_media_play);
+                    state = MEDIA_READY;
+                }
+            });
+            mediaPlayer.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
+                @Override
+                public void onBufferingUpdate(MediaPlayer mediaPlayer, int i) {
+                    progressBar.setProgress(i);
+                }
+            });
+            mediaPlayer.prepareAsync();
+        } catch (IOException e) {
+            Log.e(TAG, "Error while opening stream", e);
+        }
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (state) {
+                    case MEDIA_READY:
+                        mediaPlayer.start();
+                        state = MEDIA_PLAYING;
+                        button.setBackgroundResource(android.R.drawable.ic_media_pause);
+                        break;
+                    case MEDIA_PLAYING:
+                        mediaPlayer.pause();
+                        state = MEDIA_READY;
+                        button.setBackgroundResource(android.R.drawable.ic_media_play);
+                    default:
+                        break;
+                }
+            }
+        });
     }
 }
