d2215001a9ca13dc8fb003bda63050325d297377
dsvoronin <dsvoronin@gmail.com>
2012-02-15 23:55:30 +0400

    Добавлено главное меню

--- src/com/dsvoronin/grindfm/GrindActivity.java
+++ src/com/dsvoronin/grindfm/GrindActivity.java
@@ -6,33 +6,38 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
-import android.widget.Button;
-import android.widget.ProgressBar;
+import android.widget.*;
 import com.dsvoronin.R;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SplashActivity extends Activity {
+public class GrindActivity extends Activity {
 
     private static final int MEDIA_NOT_READY = 0;
     private static final int MEDIA_READY = 1;
     private static final int MEDIA_PLAYING = 2;
 
-    private static final String TAG = SplashActivity.class.getSimpleName();
+    private static final String TAG = GrindActivity.class.getSimpleName();
 
     private int state = MEDIA_NOT_READY;
     private Button button;
     private ProgressBar progressBar;
+    private HorizontalListView listMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.splash);
+        setContentView(R.layout.grind);
 
         progressBar = (ProgressBar) findViewById(R.id.progressbar);
         button = (Button) findViewById(R.id.button);
+        listMenu = (HorizontalListView) findViewById(R.id.main_menu);
+        listMenu.setAdapter(mAdapter);
 
         final MediaPlayer mediaPlayer = new MediaPlayer();
         try {
@@ -70,4 +75,61 @@ public class SplashActivity extends Activity {
             }
         });
     }
+
+    private class MenuItem {
+        private int resId;
+        private String title;
+
+        private MenuItem(int resId, String title) {
+            this.resId = resId;
+            this.title = title;
+        }
+
+        public int getResId() {
+            return resId;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+    }
+
+    private List<MenuItem> menuItemList = new ArrayList<MenuItem>() {
+        {
+            add(new MenuItem(R.drawable.microphone, "Радио"));
+            add(new MenuItem(R.drawable.calendar, "Расписание"));
+            add(new MenuItem(R.drawable.news, "Новости"));
+            add(new MenuItem(R.drawable.headphones, "Заказ песен"));
+            add(new MenuItem(R.drawable.youtube_play_icon, "Видео"));
+        }
+    };
+
+    private BaseAdapter mAdapter = new BaseAdapter() {
+
+        @Override
+        public int getCount() {
+            return menuItemList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View retval = getLayoutInflater().inflate(R.layout.listmenu_item, null);
+            TextView title = (TextView) retval.findViewById(R.id.title);
+            title.setText(menuItemList.get(position).getTitle());
+            ImageView imageView = (ImageView) retval.findViewById(R.id.image);
+            imageView.setImageResource(menuItemList.get(position).getResId());
+            return retval;
+        }
+
+    };
 }
