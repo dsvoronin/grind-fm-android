b931f76f4f736b45dc3f3553432b6fa6f4291b93
Dmitriy Voronin <dsvoronin@gmail.com>
2012-07-08 19:32:27 +0400

    firstmacbuild

--- src/com/dsvoronin/grindfm/GrindApplication.java
+++ src/com/dsvoronin/grindfm/GrindApplication.java
@@ -3,11 +3,15 @@ package com.dsvoronin.grindfm;
 import android.app.Application;
 import android.os.Environment;
 import com.dsvoronin.grindfm.util.ImageManager;
+import org.acra.ACRA;
+import org.acra.annotation.ReportsCrashes;
 
+@ReportsCrashes(formKey = "dGlWLTc2OXZKSWlwNWxkZWs1cUZ3Y3c6MQ")
 public class GrindApplication extends Application {
 
     @Override
     public void onCreate() {
+        ACRA.init(this);
         super.onCreate();
         String cacheDir = Environment.getExternalStorageDirectory().getPath() + "/Android/data/" + getPackageName() + "/cache/";
         ImageManager imageManager = ImageManager.getInstance();
