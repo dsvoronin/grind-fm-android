ad40167f78e0419fcc3024a94e93bf0a474a8aed
Dmitriy Voronin <dvoronin@avito.ru>
2016-04-26 22:48:57 +0300

    notification for player. player rewritten with inspiration from UMP from google

---
+++ GrindFM/src/main/java/com/dsvoronin/grindfm/sync/CurrentTrackAsyncTask.java
@@ -0,0 +1,12 @@
+package com.dsvoronin.grindfm.sync;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.dsvoronin.grindfm.App;
+import com.dsvoronin.grindfm.entities.CurrentTrack;
+
+import java.io.IOException;
+
+
